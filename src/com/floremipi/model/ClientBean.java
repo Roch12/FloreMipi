5329458153229d8d42997e74908f78dac76774d2
Maxime <maxime-roch@y-nov.com>
2014-12-19 18:24:21 +0100

    MRO - Managed Bean

--- src/com/floremipi/model/ClientBean.java
+++ src/com/floremipi/model/ClientBean.java
@@ -2,6 +2,7 @@ package com.floremipi.model;
 
 import java.util.ArrayList;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
@@ -15,13 +16,18 @@ public class ClientBean {
 
 	
 	public ClientBean() {
-		clientList = new ArrayList<Client>();
-		clientList.add(new Client(0, "Maxime", "Roch", "email", "phone"));
-		clientList.add(new Client(1, "Maxime", "Roch", "email", "phone"));
-		clientList.add(new Client(3, "Maxime", "Roch", "email", "phone"));
-		clientList.add(new Client(6, "Maxime", "Roch", "email", "phone"));
+		
 	}
 	
+	@PostConstruct
+	public void Init(){
+		clientList = new ArrayList<Client>();
+		clientList.add(new Client(0, "Maxime", "Roch", "maxime.roch@y-nov.com", "0655555555"));
+		clientList.add(new Client(1, "Johan", "Pouget", "johan.pouget@y-nov.com", "0644444444"));
+		clientList.add(new Client(2, "Morgan", "Billon", "morgan-billon@y-nov.com", "0633333333"));
+		clientList.add(new Client(3, "Lucas", "Brettes", "lucas.brettes@y-nov.com", "0622222222"));
+		clientList.add(new Client(3, "Swann", "Waselin", "swann.vaselin@y-nov.com", "0611111111"));
+	}
 	
 	/**
 	 * @return the clientList
---
+++ src/com/floremipi/model/Stock.java
@@ -0,0 +1,11 @@
+package com.floremipi.model;
+
+public class Stock {
+
+	public Integer id;
+	public String name;
+	public String description;
+	public String categorie;
+	public String imgSrc;
+	public Integer qteStock;
+}
